484d0659c05ad4b813eceb0593b74b5ba29c6877
TomDal <lennox77777@gmail.com>
2022-07-12 15:16:21 +0200

    Commit1

---
+++ src/Wisielec.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Wisielec extends JFrame {
+
+public Wisielec (){
+JFrame frame = new JFrame();
+frame.setLayout(new BorderLayout());
+frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+frame.setSize(800, 800);
+frame.setBackground(Color.lightGray);
+frame.setVisible(true);
+
+}
+
+}
